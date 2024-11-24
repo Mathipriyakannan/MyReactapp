eca0a4dd12089f557a5f302762fbe27d2230e7ab
Mathipriya.K <148265131+Mathipriyakannan@users.noreply.github.com>
2024-11-24 12:25:32 +0530

    Add files via upload

---
+++ Phone.js
@@ -0,0 +1,71 @@
+import React from "react";
+import'./phone.css';
+
+function Phone (){
+        return(
+        <div>
+<div className="containe-fuild d-flex" style={{marginTop:"50px"}}>
+    <img src="./images/ph img.jfif" alt="#" />
+    <p className="sam"> SAMSUNG Galaxy A14 5G<br></br>
+    (Dark,Red 128 GB)<br></br>
+    #JustHere<br></br><br></br>
+     <i class="bi bi-star ms-1" style={{color:"darkgreen"}}></i><i class="bi bi-star ms-1" style={{color:"darkgreen"}}></i><i class="bi bi-star ms-1" style={{color:"darkgreen"}}></i><i class="bi bi-star ms-1"style={{color:"darkgreen"}}></i><i class="bi bi-star-half ms-1"style={{color:"darkgreen"}}></i>(49,567)<br></br>  
+    <h6><span className="col"> 45% off</span> 20,999 <i class="bi bi-currency-rupee"></i>11,499</h6>
+    <span className="col">Extra <i class="bi bi-currency-rupee"></i>750 off with Airtel offer</span><br></br>
+       <h6>Up <i class="bi bi-currency-rupee"></i><span className="color">9,300 </span>off on Exchange</h6>
+    <h6><i class="bi bi-truck-flatbed"></i>  <span className="color">EXPRESS </span>2day delivery,sunday</h6>
+    1 year wattanty by SAMSUNG</p></div>
+
+
+
+<div className="containe-fuild d-flex" style={{marginTop:"50px"}}>
+
+    <img src="./images/ph img1.jfif" alt="#" />
+    <p className="sam"> SAMSUNG Galaxy F15 5G<br></br>
+    (Groovy Violet128 GB)<br></br>
+    #JustHere<br></br><br></br>
+     <i class="bi bi-star ms-1" style={{color:"darkgreen"}}></i><i class="bi bi-star ms-1" style={{color:"darkgreen"}}></i><i class="bi bi-star ms-1" style={{color:"darkgreen"}}></i><i class="bi bi-star ms-1"style={{color:"darkgreen"}}></i><i class="bi bi-star-half ms-1"style={{color:"darkgreen"}}></i>(49,567)<br></br>  
+    <h6><span className="col"> 14% off</span> 16,999<i class="bi bi-currency-rupee"></i>14,949</h6>
+    <span className="col">Extra <i class="bi bi-currency-rupee"></i>750 off with Airtel offer</span><br></br>
+       <h6>Upto <i class="bi bi-currency-rupee"></i><span className="color">10,350 </span>off on Exchange</h6>
+    <h6><i class="bi bi-truck-flatbed"></i>  <span className="color">EXPRESS </span>2day delivery,sunday</h6>
+    1 year wattanty by SAMSUNG</p></div>
+
+    <div className="containe-fuild d-flex" style={{marginTop:"50px"}}>
+
+<img src="./images/ph img2.jfif" alt="#" />
+<p className="sam"> SAMSUNG Galaxy A15 5G<br></br>
+(Blue Black,128 GB)<br></br>
+#JustHere<br></br><br></br>
+ <i class="bi bi-star ms-1" style={{color:"darkgreen"}}></i><i class="bi bi-star ms-1" style={{color:"darkgreen"}}></i><i class="bi bi-star ms-1" style={{color:"darkgreen"}}></i><i class="bi bi-star ms-1"style={{color:"darkgreen"}}></i><i class="bi bi-star-half ms-1"style={{color:"darkgreen"}}></i>(49,567)<br></br>  
+<h6><span className="col"> 21% off</span> 21,499<i class="bi bi-currency-rupee"></i>16,890</h6>
+<span className="col"> Save Extra with combo offer<br></br> Bank Offer</span><br></br>
+<h6>Free delivery by 20th Nov</h6>
+1 year wattanty by SAMSUNG</p></div>
+
+<div className="containe-fuild d-flex" style={{marginTop:"50px"}}>
+
+    <img src="./images/ph img3.jfif" alt="#" />
+    <p className="sam"> SAMSUNG Galaxy M3 5G<br></br>
+    (Moonlight Blue,128 GB)<br></br>
+
+     <i class="bi bi-star ms-1" style={{color:"darkgreen"}}></i><i class="bi bi-star ms-1" style={{color:"darkgreen"}}></i><i class="bi bi-star ms-1" style={{color:"darkgreen"}}></i><i class="bi bi-star ms-1"style={{color:"darkgreen"}}></i><i class="bi bi-star-half ms-1"style={{color:"darkgreen"}}></i>(49,567)<br></br>  
+    <h6><span className="col"> 34% off</span> 20,999<i class="bi bi-currency-rupee"></i>15,925</h6>
+    <span className="col">Extra with combo offer</span><br></br>
+       <h6><span className="red">Only 6 left </span></h6>
+    1 Free delivery by 20th Nov</p></div>
+
+
+
+
+
+
+
+</div>
+
+
+   
+
+    )
+}
+export default Phone;
